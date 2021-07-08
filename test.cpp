c189ef83d78b89784343af6ef6f36f0778fd800b
Juntaro Otaka <nijirigoke173923@gmail.com>
2021-07-08 11:01:05 +0900

    - README.mdを追記。 - ロボットの初期化ロジック、for文の書き方をc++11向けにアップデート - 乱数生成を信頼性の高いメルセンヌツイスタに変更

--- test.cpp
+++ test.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 //#include "simbase.h"
 #include "workspace.h"
+#include <random>
 
 //pull request test
 #define RED 0
@@ -16,19 +17,19 @@
 #define CENTER 1
 #define RIGHT  2
 #define TRANGE 1.5 //タッチセンサーのレンジ 半径の倍数
-#define RANGE 30 //通信レンジ の半径
+#define RANGE 50 //通信レンジ の半径
 
 
 #define RIGHT_TURN -0.1        //右回転 0.1ラジアンの定義
 #define LEFT_TURN    0.1        //左回転 0.1ラジアンの定義
-#define ROBOS  100   //ロボット台数　10台
+#define ROBOS  3   //ロボット台数　10台
 #define NSENSORRANGE 100    //センサーの検出距離（絶対距離）
 
 typedef struct ROBO {
-    double r;
-    double x, y;
-    double dir;
-    POSITION tsensor[3]; //構造体変数の追加
+    double r{};
+    double x{}, y{};
+    double dir{};
+    POSITION tsensor[3]{}; //構造体変数の追加
 public:
     void draw();
 
@@ -48,7 +49,7 @@ public:
 
     int check_cross_others(POSITION p);
 
-    int SearchRobot(POSITION p, double range);
+//    int SearchRobot(POSITION p, double range);
 
     int stack = 0;
 } ROBO;
@@ -62,9 +63,9 @@ void Initialize();
 void wall_draw() {
 
     glBegin(GL_LINES);
-    for (int i = 0; i < CIRCLEDIV; i++) {
-        glVertex2d(pin[wall[i].p1].x, pin[wall[i].p1].y);
-        glVertex2d(pin[wall[i].p2].x, pin[wall[i].p2].y);
+    for (auto &i : wall) {
+        glVertex2d(pin[i.p1].x, pin[i.p1].y);
+        glVertex2d(pin[i.p2].x, pin[i.p2].y);
     }
     glEnd();
 }
@@ -73,8 +74,8 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     graphics();
     wall_draw();
-    for (int i = 0; i < ROBOS; i++) {
-        robo[i].draw();
+    for (auto &i : robo) {
+        i.draw();
     }
 
     glutSwapBuffers();
@@ -126,9 +127,7 @@ void ROBO::action() {
 void idle() {
     if (fStart == 0)
         return;
-    for (int i = 0; i < ROBOS; i++) {
-        robo[i].action();
-    }
+    for (auto &i : robo) i.action();
     Sleep(1 * 5);
     display();
 }
@@ -140,16 +139,22 @@ void mouse(int button, int state, int x, int y) //マウスボタンの処理
             fStart = 0;
         else
             fStart = 1;
-        for (int i = 0; i < ROBOS; i++) {
-            robo[i].init();
+        for (auto &i : robo) {
+            i.init();
         }
     }
 }
 
 void ROBO::init() {
-    dir = rand() * 360.0;
-    x = (rand() % 400)-200;
-    y = (rand() % 400)-200;
+    std::random_device rnd;     // 非決定的な乱数生成器
+    std::mt19937 mt(rnd());
+    std::uniform_int_distribution<int> distr(-200, 200);    // 非決定的な乱数生成器
+    std::uniform_real_distribution<> dir_gen(0, 360);
+
+    dir = dir_gen(mt);
+    printf("%f", dir);
+    x = distr(mt);
+    y = distr(mt);
 
 //    x =  250;
 //    y =  250;
@@ -168,6 +173,7 @@ void ROBO::draw() {
     glTranslated(x, y, 0);              //ロボットの現在座標へ座標系をずらす
     glRotated(dir / PI * 180, 0, 0, 1); //進行方向へZ軸回転
     draw_circle(0, 0, r); //本体外形円の描画　現在の座標系の原点に対して描くことに注意
+    draw_circle(0, 0, RANGE); //通信範囲の描画
 
     glBegin(GL_LINES);
     glVertex2d(0, 0); //左センサーの描画
@@ -217,7 +223,7 @@ int ROBO::touchsensor(int i)
 
 int ROBO::check_cross_wall(POSITION p1, POSITION p2) {
 
-    for (int i = 0; i < CIRCLEDIV; i++) {
+    for (auto &i : wall) {
         double Wp1x, Wp1y, Wp2x, Wp2y; //線分の両端点
         double Bvx, Bvy;               //線分への垂線の方向ベクトル
         double Bx, By, R;              //球体の位置, 半径
@@ -227,10 +233,10 @@ int ROBO::check_cross_wall(POSITION p1, POSITION p2) {
         Bx = p1.x;
         By = p1.y;
 
-        Wp1x = pin[wall[i].p1].x;
-        Wp1y = pin[wall[i].p1].y;
-        Wp2x = pin[wall[i].p2].x;
-        Wp2y = pin[wall[i].p2].y;
+        Wp1x = pin[i.p1].x;
+        Wp1y = pin[i.p1].y;
+        Wp2x = pin[i.p2].x;
+        Wp2y = pin[i.p2].y;
 
         Bvx = (p2.x - p1.x);
         Bvy = (p2.y - p1.y);
@@ -264,9 +270,9 @@ int ROBO::check_cross_others(POSITION p) {
 // 区別したらそいつとの距離を産出ること
     sensor_x = p.x;
     sensor_y = p.y;
-    for (int i = 0; i < ROBOS; ++i) {
-        double another_robo_x = robo[i].x;
-        double another_robo_y = robo[i].y;
+    for (auto &i : robo) {
+        double another_robo_x = i.x;
+        double another_robo_y = i.y;
         double distance_x;
         double distance_y;
 
@@ -275,24 +281,24 @@ int ROBO::check_cross_others(POSITION p) {
 
         l = sqrt(pow(distance_x, 2) + pow(distance_y, 2));
 
-        if (l < robo[i].r) {
+        if (l < i.r) {
             return 1;
         }
     }
     return 0;
 }
 
-double ROBO:: nearrobotsensor(){
+double ROBO::nearrobotsensor() {
     double l;
     double distance_x;
     double distance_y;
 
     //自身の座標から一定レンジを探索？ 計算量ちゃん…。
-    for (int i = 0; i < ROBOS; ++i) {
+    for (auto &i : robo) {
 
         //他のロボットのちゃん座標
-        double another_robo_x = robo[i].x;
-        double another_robo_y = robo[i].y;
+        double another_robo_x = i.x;
+        double another_robo_y = i.y;
 
 
         distance_x = another_robo_x - x;
@@ -300,36 +306,33 @@ double ROBO:: nearrobotsensor(){
 
         l = sqrt(pow(distance_x, 2) + pow(distance_y, 2));
 
-        if (l < robo[i].r) {
+        if (l < RANGE) {
             return 1;
         }
     }
 
 }
 
-int SearchRobot(POSITION p, double range){
-
-
-
-    return 0;
-}
-
+//int SearchRobot(POSITION p, double range) {
+//
+//
+//    return 0;
+//}
 
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < CIRCLEDIV; i++) {
-        pin[i] = {250 * sin(2*PI * (double) i / (double) CIRCLEDIV),
-                  250 * cos(2*PI * (double) i / (double) CIRCLEDIV)};
 
-//        printf("%f\n%f\n",pin[i].x,pin[i].y);
-//        printf("-------------------------\n");
+    for (int i = 0; i < CIRCLEDIV; i++) {
+        pin[i] = {250 * sin(2 * PI * (double) i / (double) CIRCLEDIV),
+                  250 * cos(2 * PI * (double) i / (double) CIRCLEDIV)};
+
+        printf("%f\n%f\n", pin[i].x, pin[i].y);
+        printf("-------------------------\n");
         wall[i] = {i + 0, i + 1};
 
-        if(i==CIRCLEDIV-1){
-            wall[i]={i,0};
-        }
-        printf("%d\n%d\n",wall[i].p1,wall[i].p2);
-        printf("-------------------------\n");
+        if (i == CIRCLEDIV - 1) wall[i] = {i, 0};
+//        printf("%d\n%d\n", wall[i].p1, wall[i].p2);
+//        printf("-------------------------\n");
 
     }
 //    pin[CIRCLEDIV-1] = {0,0};
@@ -352,7 +355,5 @@ int main(int argc, char *argv[]) {
 
 void Initialize() {
     make_circle();//円図形データの作成
-    for (int i = 0; i < ROBOS; ++i) {
-        robo[i].init();
-    }
+    for (auto &i : robo) i.init();
 }
